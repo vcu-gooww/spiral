c5ce2c3034f7e15315719732be1744b96d142fed
vcu-gooww <78395107+vcu-gooww@users.noreply.github.com>
2021-04-09 19:35:36 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,15 +1,7 @@
 def spiralize(number):
-    n = 1
-    step = 2
-    total = 0
-    since_last = 0
-    while n <= size**2:
-    total += n
-    n += step
-    since_last += 1
-    if since_last == 4:
-    step +=2
-    since_last = 0
-    return total
-print(sprialize(501))
+    return_value = 1
+    n = (number - 1) / 2
+    x = 2 * n * (8 * n * n + 15 * n + 13) / 3 + 1
+    Return_value = x
+    return return_value
 
