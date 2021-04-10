5c77b651839bdfbcad00a8a9e62fd3df083ee433
vcu-gooww <78395107+vcu-gooww@users.noreply.github.com>
2021-04-09 20:43:09 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,6 +1,6 @@
 def spiralize(number):
     n = (number - 1) // 2
-    x = (8 * n * (n + 1) * (2*n+1)) / 3 + 2 * n * (n + 1) + 4 * n + 1
+    x = (8 * n * (n + 1) * (2*n+1)) // 3 + 2 * n * (n + 1) + 4 * n + 1
     return x
 
-print(spiralize(501)) 
+print(spiralize(501)).
